5ee22462e2dc782e454e85372d8749f725d60779
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-09 21:11:58 -0300

    feat: implementing AggregateRoot on Category class

--- infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/Main.java
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/Main.java
@@ -1,7 +1,5 @@
 package com.fullcyle.admin.catalog.infastructure;
 
-import com.fullcyle.admin.catalog.application.UseCase;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");

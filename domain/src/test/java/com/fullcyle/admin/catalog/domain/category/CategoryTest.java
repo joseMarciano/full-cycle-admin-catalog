d96693119badcf2401e87c2b6e44d2dea6100edc
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-10 20:47:27 -0300

    test: ensure Category will not instanciate with  invalid empty length less than 3

--- domain/src/test/java/com/fullcyle/admin/catalog/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcyle/admin/catalog/domain/category/CategoryTest.java
@@ -74,4 +74,25 @@ public class CategoryTest {
         Assertions.assertEquals(1, actualException.getErrors().size());
 
     }
+    @Test
+    public void givenAnInvalidNameLengthLessThan3_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final var expectedName = "Fi ";
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final var expectedDescription = "The category most watched";
+        final var expectedIsActive = true;
+
+
+        final var actualCategory = Category.newCategory(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        var actualException = Assertions.assertThrows(DomainException.class, () ->
+                actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(1, actualException.getErrors().size());
+
+    }
 }

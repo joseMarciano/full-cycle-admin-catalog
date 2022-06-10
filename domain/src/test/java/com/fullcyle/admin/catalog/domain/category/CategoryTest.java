a249619ff3e1cd9ad64052e4b55733f79aee5f02
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-09 21:54:05 -0300

    test: ensure Category will validate herself with invalid name

--- domain/src/test/java/com/fullcyle/admin/catalog/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcyle/admin/catalog/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.fullcyle.admin.catalog.domain.category;
 
+import com.fullcyle.admin.catalog.domain.exceptions.DomainException;
+import com.fullcyle.admin.catalog.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +28,28 @@ public class CategoryTest {
         Assertions.assertNotNull(actualCategory.getCreatedAt());
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
+
+    }
+
+    @Test
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = null;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "The category most watched";
+        final var expectedIsActive = true;
+
+
+        final var actualCategory = Category.newCategory(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        var actualException = Assertions.assertThrows(DomainException.class, () ->
+                actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(1, actualException.getErrors().size());
 
     }
 }

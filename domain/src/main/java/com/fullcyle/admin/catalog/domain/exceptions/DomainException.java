b01f96cdb3bc536c84c95d1bcf98ca31f39bd2ba
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-10 20:12:20 -0300

    refactor: refining Exceptions

--- domain/src/main/java/com/fullcyle/admin/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/fullcyle/admin/catalog/domain/exceptions/DomainException.java
@@ -4,17 +4,21 @@ import com.fullcyle.admin.catalog.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStackTraceException {
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrors) {
-        super("", null, true, false);
+    private DomainException(final String aMessage, List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
     public static DomainException with(final List<Error> anErrors) {
-        return new DomainException(anErrors);
+        return new DomainException(anErrors != null ? anErrors.get(0).message() : "", anErrors);
+    }
+
+    public static DomainException with(final Error anError) {
+        return DomainException.with(List.of(anError));
     }
 
     public List<Error> getErrors() {
---
+++ domain/src/main/java/com/fullcyle/admin/catalog/domain/exceptions/NoStackTraceException.java
@@ -0,0 +1,12 @@
+package com.fullcyle.admin.catalog.domain.exceptions;
+
+public class NoStackTraceException extends RuntimeException {
+
+    public NoStackTraceException(String message) {
+        super(message, null);
+    }
+
+    public NoStackTraceException(String message, Throwable cause) {
+        super(message, cause, true, false);
+    }
+}

09660e4db58b16acc02cf5356f115802a982d64c
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-10 21:39:30 -0300

    feat: create Category Gateway

---
+++ domain/src/main/java/com/fullcyle/admin/catalog/domain/category/CategoryGateway.java
@@ -0,0 +1,19 @@
+package com.fullcyle.admin.catalog.domain.category;
+
+import com.fullcyle.admin.catalog.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    Category deleteById(CategoryID anID);
+
+    Optional<Category> findById(CategoryID anID);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+
+}

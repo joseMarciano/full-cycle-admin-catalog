d2469c979ce076a5142cc820a1cb2ae84b206397
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-14 23:08:48 -0300

    test: ensure CreateCategoryUseCase returns DomaiException with invalid name

--- application/src/test/java/com/fullcyle/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcyle/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
@@ -2,6 +2,7 @@ package com.fullcyle.admin.catalog.application.category.create;
 
 
 import com.fullcyle.admin.catalog.domain.category.CategoryGateway;
+import com.fullcyle.admin.catalog.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
@@ -22,6 +24,7 @@ public class CreateCategoryUseCaseTest {
 
     @Mock
     private CategoryGateway categoryGateway;
+
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
         final var expectedName = "Filmes";
@@ -40,6 +43,55 @@ public class CreateCategoryUseCaseTest {
         verify(categoryGateway, times(1)).create(argThat(aCategory ->
                 Objects.equals(expectedName, aCategory.getName())
                         && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedActive, aCategory.isActive())
+                        && Objects.nonNull(aCategory.getId())
+                        && Objects.nonNull(aCategory.getCreatedAt())
+                        && Objects.nonNull(aCategory.getUpdatedAt())
+                        && Objects.isNull(aCategory.getDeletedAt())
+
+        ));
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateCategory_shouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "Most watched";
+        final var expectedActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var aCommand =
+                CreateCategoryCommand.with(expectedName, expectedDescription, expectedActive);
+
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> useCase.execute(aCommand));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        verify(categoryGateway, times(0)).create(any());
+
+    }
+
+    @Test
+    public void givenAValidCommandWithInactiveCategory_whenCallsCreateCategory_shouldReturnInactiveCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Most watched";
+        final var expectedActive = true;
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedActive);
+
+        when(categoryGateway.create(any())).thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        verify(categoryGateway, times(1)).create(argThat(aCategory ->
+                Objects.equals(expectedName, aCategory.getName())
+                        && Objects.equals(expectedDescription, aCategory.getDescription())
+                        && Objects.equals(expectedActive, aCategory.isActive())
                         && Objects.nonNull(aCategory.getId())
                         && Objects.nonNull(aCategory.getCreatedAt())
                         && Objects.nonNull(aCategory.getUpdatedAt())

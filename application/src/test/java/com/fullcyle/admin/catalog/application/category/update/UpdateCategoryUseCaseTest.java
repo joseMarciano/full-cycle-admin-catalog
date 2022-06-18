e3f1c4321132f732ff54f499501b69d4e0cd2d27
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-18 12:09:09 -0300

    feat: unsure UpdateCategoryUseCase will update correctly

---
+++ application/src/test/java/com/fullcyle/admin/catalog/application/category/update/UpdateCategoryUseCaseTest.java
@@ -0,0 +1,78 @@
+package com.fullcyle.admin.catalog.application.category.update;
+
+import com.fullcyle.admin.catalog.domain.category.Category;
+import com.fullcyle.admin.catalog.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultUpdateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {
+        final var aCategory = Category.newCategory(
+                "Film",
+                null,
+                true
+        );
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Most watched";
+        final var expectedActive = true;
+        final var expectedId = aCategory.getId();
+
+
+        final var aCommand = UpdateCategoryCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedDescription,
+                expectedActive
+        );
+
+        when(categoryGateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(aCategory.clone()));
+
+        when(categoryGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+
+        final var actualOutput = useCase.execute(aCommand).get();
+
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
+
+        verify(categoryGateway, times(1)).findById(eq(expectedId));
+        verify(categoryGateway, times(1)).update(argThat(
+                aUpdatedCategory ->
+                        Objects.equals(expectedName, aUpdatedCategory.getName())
+                                && Objects.equals(expectedDescription, aUpdatedCategory.getDescription())
+                                && Objects.equals(expectedActive, aUpdatedCategory.isActive())
+                                && Objects.equals(expectedId, aUpdatedCategory.getId())
+                                && Objects.equals(aCategory.getCreatedAt(), aUpdatedCategory.getCreatedAt())
+                                && Objects.nonNull(aCategory.getUpdatedAt())
+//                                && Objects.equals(aCategory.getUpdatedAt(), aUpdatedCategory.getUpdatedAt())
+                                && aCategory.getCreatedAt().isBefore(aUpdatedCategory.getUpdatedAt())
+                                && Objects.isNull(aCategory.getDeletedAt())
+        ));
+
+
+    }
+
+}

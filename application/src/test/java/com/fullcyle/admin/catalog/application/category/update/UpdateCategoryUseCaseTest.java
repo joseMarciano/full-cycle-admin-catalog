030b770d37d3e301ed8acf67e4d326120379dfbe
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-18 12:13:24 -0300

    test: unsure UpdateCategoryUseCase will return a Notification if invalid name is passed

--- application/src/test/java/com/fullcyle/admin/catalog/application/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcyle/admin/catalog/application/category/update/UpdateCategoryUseCaseTest.java
@@ -1,5 +1,6 @@
 package com.fullcyle.admin.catalog.application.category.update;
 
+import com.fullcyle.admin.catalog.application.category.create.CreateCategoryCommand;
 import com.fullcyle.admin.catalog.domain.category.Category;
 import com.fullcyle.admin.catalog.domain.category.CategoryGateway;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Objects;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.eq;
@@ -67,11 +69,45 @@ public class UpdateCategoryUseCaseTest {
                                 && Objects.equals(expectedId, aUpdatedCategory.getId())
                                 && Objects.equals(aCategory.getCreatedAt(), aUpdatedCategory.getCreatedAt())
                                 && Objects.nonNull(aCategory.getUpdatedAt())
-//                                && Objects.equals(aCategory.getUpdatedAt(), aUpdatedCategory.getUpdatedAt())
                                 && aCategory.getCreatedAt().isBefore(aUpdatedCategory.getUpdatedAt())
                                 && Objects.isNull(aCategory.getDeletedAt())
         ));
+    }
 
+    @Test
+    public void givenAInvalidName_whenCallsUpdateCategory_shouldReturnDomainException() {
+        final var aCategory = Category.newCategory(
+                "Film",
+                null,
+                true
+        );
+
+
+        final String expectedName = null;
+        final var expectedDescription = "Most watched";
+        final var expectedActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+        final var expectedId = aCategory.getId();
+
+
+        final var aCommand =
+                UpdateCategoryCommand.with(
+                        expectedId.getValue(),
+                        expectedName,
+                        expectedDescription,
+                        expectedActive);
+
+        when(categoryGateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(aCategory.clone()));
+
+        final var notification = useCase.execute(aCommand).getLeft();
+
+        assertEquals(expectedErrorMessage, notification.firstError().message());
+        assertEquals(expectedErrorCount, notification.getErrors().size());
+
+        verify(categoryGateway, times(1)).findById(eq(expectedId));
+        verify(categoryGateway, times(0)).update(any());
 
     }
 

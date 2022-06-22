0179413d7d2565faee652fddb5415685d918146e
joseMarciano <marcianojosepaulo@gmail.com>
2022-06-21 21:54:22 -0300

    chore: add spring dependencies on infrastructure layer

---
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.fullcyle.admin.catalog.infastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcyle.admin.catalog")
+public class WebServerConfig {
+}

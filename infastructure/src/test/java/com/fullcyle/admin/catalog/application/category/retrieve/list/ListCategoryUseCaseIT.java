608334e30993ae29382593339c2d5c73fba1cdd8
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-04 21:55:02 -0300

    test: create testIT for pagination of Categories

--- infastructure/src/test/java/com/fullcyle/admin/catalog/application/category/retrieve/list/ListCategoryUseCaseIT.java
+++ infastructure/src/test/java/com/fullcyle/admin/catalog/application/category/retrieve/list/ListCategoryUseCaseIT.java
@@ -106,4 +106,83 @@ public class ListCategoryUseCaseIT {
         assertEquals(expectedCategoryName, actualResult.items().get(0).name());
 
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "name,asc,0,10,7,7,Amazon Originals",
+            "name,desc,0,10,7,7,Sports",
+            "createdAt,asc,0,10,7,7,Filmes",
+            "createdAt,desc,0,10,7,7,Series",
+    })
+    public void givenAValidSortAndDirection_whenCallsListCategories_thenShouldReturnCategoriesOrdered(
+            final String expectedSort,
+            final String expectedDirection,
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedItemsCount,
+            final long expectedTotal,
+            final String expectedCategoryName
+    ){
+
+        final var expectedTerms = "";
+
+        final var aQuery = new CategorySearchQuery(
+                expectedPage,
+                expectedPerPage,
+                expectedTerms,
+                expectedSort,
+                expectedDirection
+        );
+
+        final var actualResult = useCase.execute(aQuery);
+
+        assertEquals(expectedItemsCount, actualResult.items().size());
+        assertEquals(expectedPage, actualResult.currentPage());
+        assertEquals(expectedPerPage, actualResult.perPage());
+        assertEquals(expectedTotal, actualResult.total());
+        assertEquals(expectedCategoryName, actualResult.items().get(0).name());
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,2,2,7,Amazon Originals;Documentarios",
+            "1,2,2,7,Filmes;Kids",
+            "2,2,2,7,NetFlix;Series",
+            "3,2,1,7,Sports",
+    })
+    public void givenAValidPage_whenCallsListCategories_shouldReturnCategoriesPaginated(
+            final int expectedPage,
+            final int expectedPerPage,
+            final int expectedItemsCount,
+            final long expectedTotal,
+            final String expectedCategoriesName
+    ){
+
+        final var expectedTerms = "";
+        final var expectedSort = "name";
+        final var expectedDirection = "asc";
+
+        final var aQuery = new CategorySearchQuery(
+                expectedPage,
+                expectedPerPage,
+                expectedTerms,
+                expectedSort,
+                expectedDirection
+        );
+
+        final var actualResult = useCase.execute(aQuery);
+
+        assertEquals(expectedItemsCount, actualResult.items().size());
+        assertEquals(expectedPage, actualResult.currentPage());
+        assertEquals(expectedPerPage, actualResult.perPage());
+        assertEquals(expectedTotal, actualResult.total());
+
+        int index = 0;
+        for (String expectedName : expectedCategoriesName.split(";")) {
+            final var actualName = actualResult.items().get(index).name();
+            assertEquals(expectedName, actualName);
+            index++;
+        }
+    }
 }

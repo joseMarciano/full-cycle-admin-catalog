f7be55600f683d2f95fa3465b904da6b5991c288
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-06 23:41:07 -0300

    test: create Category end-point

---
+++ infastructure/src/test/java/com/fullcyle/admin/catalog/infastructure/api/CategoryAPITest.java
@@ -0,0 +1,67 @@
+package com.fullcyle.admin.catalog.infastructure.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fullcyle.admin.catalog.ControllerTest;
+import com.fullcyle.admin.catalog.application.category.create.CreateCategoryOutput;
+import com.fullcyle.admin.catalog.application.category.create.CreateCategoryUseCase;
+import com.fullcyle.admin.catalog.infastructure.category.models.CreateCategoryApiInput;
+import io.vavr.API;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Objects;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ControllerTest(controllers = CategoryAPI.class)
+public class CategoryAPITest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private CreateCategoryUseCase createCategoryUseCase;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Most watched";
+        final var expectedActive = true;
+
+        final var aCommand = new CreateCategoryApiInput(expectedName, expectedDescription, expectedActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(API.Right(CreateCategoryOutput.from("123")));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aCommand));
+
+        this.mvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpectAll(
+                        MockMvcResultMatchers.status().isCreated(),
+                        MockMvcResultMatchers.header().string("Location", "/categories/123"),
+                        MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE),
+                        MockMvcResultMatchers.jsonPath("$.id", Matchers.equalTo("123"))
+                );
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedActive, cmd.isActive())
+        ));
+    }
+}

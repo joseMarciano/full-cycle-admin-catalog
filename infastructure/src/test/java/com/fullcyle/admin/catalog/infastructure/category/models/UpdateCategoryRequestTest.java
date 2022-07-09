2c9baad1ea0399031827f23739932b5453f66a7c
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-09 15:13:31 -0300

    test: testing serialization of UpdateCategoryRequest

---
+++ infastructure/src/test/java/com/fullcyle/admin/catalog/infastructure/category/models/UpdateCategoryRequestTest.java
@@ -0,0 +1,45 @@
+package com.fullcyle.admin.catalog.infastructure.category.models;
+
+import com.fullcyle.admin.catalog.JacksonTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@JacksonTest
+public class UpdateCategoryRequestTest {
+
+    @Autowired
+    private JacksonTester<UpdateCategoryRequest> json;
+
+
+    @Test
+    public void testUnMarshall() throws IOException {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+
+        final var json = """
+                {
+                    "name": "%s",
+                    "description": "%s",
+                    "is_active": "%s"
+                }
+                """.formatted(
+                expectedName,
+                expectedDescription,
+                expectedIsActive);
+
+        final var actualJson = this.json.parse(json);
+
+        Assertions.assertThat(actualJson)
+                .hasFieldOrPropertyWithValue("name", expectedName)
+                .hasFieldOrPropertyWithValue("description", expectedDescription)
+                .hasFieldOrPropertyWithValue("active", expectedIsActive);
+
+    }
+}

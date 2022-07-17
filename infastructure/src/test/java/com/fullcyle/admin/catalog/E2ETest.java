f5da88b2216a85f1665e703beb36329c91c79a00
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-17 14:23:51 -0300

    fix: put AutoConfigureMockMvc to work with MockMvc tests

--- infastructure/src/test/java/com/fullcyle/admin/catalog/E2ETest.java
+++ infastructure/src/test/java/com/fullcyle/admin/catalog/E2ETest.java
@@ -2,6 +2,7 @@ package com.fullcyle.admin.catalog;
 
 import com.fullcyle.admin.catalog.infastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -13,5 +14,6 @@ import java.lang.annotation.*;
 @SpringBootTest(classes = WebServerConfig.class)
 @ExtendWith(MySQLCleanUpExtension.class)
 @ActiveProfiles("test-e2e")
+@AutoConfigureMockMvc
 public @interface E2ETest {
 }

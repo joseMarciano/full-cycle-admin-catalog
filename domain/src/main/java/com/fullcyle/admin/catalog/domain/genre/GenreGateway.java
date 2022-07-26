4a14c0d7a020a7d492788df4b6bd95c3bbfb182b
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-25 23:08:59 -0300

    feat: :sparkles: create GenreGateway

---
+++ domain/src/main/java/com/fullcyle/admin/catalog/domain/genre/GenreGateway.java
@@ -0,0 +1,19 @@
+package com.fullcyle.admin.catalog.domain.genre;
+
+import com.fullcyle.admin.catalog.domain.pagination.Pagination;
+import com.fullcyle.admin.catalog.domain.pagination.SearchQuery;
+
+import java.util.Optional;
+
+public interface GenreGateway {
+
+    Genre create(Genre aGenre);
+
+    void deleteById(GenreID anID);
+
+    Optional<Genre> findById(GenreID anId);
+
+    Genre update(Genre aGenre);
+
+    Pagination<Genre> findAll(SearchQuery aQuery);
+}

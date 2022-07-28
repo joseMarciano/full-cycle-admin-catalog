549b7d02a52a5d6891a347adaa9e33d4c8cbcdf6
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-28 20:13:39 -0300

    feat: :sparkles: create a implementation of DefaultGetGenreByIdUseCase

---
+++ application/src/main/java/com/fullcyle/admin/catalog/application/genre/retrieve/get/DefaultGetGenreByIdUseCase.java
@@ -0,0 +1,25 @@
+package com.fullcyle.admin.catalog.application.genre.retrieve.get;
+
+import com.fullcyle.admin.catalog.domain.exceptions.NotFoundException;
+import com.fullcyle.admin.catalog.domain.genre.Genre;
+import com.fullcyle.admin.catalog.domain.genre.GenreGateway;
+import com.fullcyle.admin.catalog.domain.genre.GenreID;
+
+import java.util.Objects;
+
+public class DefaultGetGenreByIdUseCase extends GetGenreByIdUseCase {
+
+    private final GenreGateway genreGateway;
+
+    public DefaultGetGenreByIdUseCase(GenreGateway genreGateway) {
+        this.genreGateway = Objects.requireNonNull(genreGateway);
+    }
+
+    @Override
+    public GenreOutput execute(String anIn) {
+        final var aGenreId = GenreID.from(anIn);
+        return this.genreGateway.findById(aGenreId)
+                .map(GenreOutput::from)
+                .orElseThrow(() -> NotFoundException.with(Genre.class, aGenreId));
+    }
+}
---
+++ application/src/main/java/com/fullcyle/admin/catalog/application/genre/retrieve/get/GetGenreByIdUseCase.java
@@ -0,0 +1,5 @@
+package com.fullcyle.admin.catalog.application.genre.retrieve.get;
+
+import com.fullcyle.admin.catalog.application.UseCase;
+
+public abstract class GetGenreByIdUseCase extends UseCase<String, GenreOutput> {}

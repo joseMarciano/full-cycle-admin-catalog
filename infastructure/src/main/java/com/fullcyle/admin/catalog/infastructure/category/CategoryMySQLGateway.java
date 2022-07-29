294e573127a09f35bf121a908ba89a062a457e3e
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-29 13:46:59 -0300

    refactor: :hammer: change @Service to @Component.
    Doesn't change the behavior but we assume that @Service we have a bussiness rules.

--- infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/category/CategoryMySQLGateway.java
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/category/CategoryMySQLGateway.java
@@ -12,13 +12,13 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Component
 public class CategoryMySQLGateway implements CategoryGateway {
 
     private final CategoryRepository repository;

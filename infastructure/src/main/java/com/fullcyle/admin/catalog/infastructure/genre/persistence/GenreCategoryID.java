f06b6577e3dfc3e7c37e2e6cb6dbb9b6c1087021
joseMarciano <marcianojosepaulo@gmail.com>
2022-07-28 21:49:20 -0300

    feat: :sparkles: create JpaEntity of Genre

---
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/genre/persistence/GenreCategoryID.java
@@ -0,0 +1,59 @@
+package com.fullcyle.admin.catalog.infastructure.genre.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class GenreCategoryID {
+
+    @Column(name = "ID_GENRES", nullable = false)
+    private String genreId;
+
+    @Column(name = "ID_CATEGORIES", nullable = false)
+    private String categoryId;
+
+    private GenreCategoryID() {
+    }
+
+    private GenreCategoryID(final String aGenreId, final String aCategoryId) {
+        this.genreId = aGenreId;
+        this.categoryId = aCategoryId;
+    }
+
+    public static GenreCategoryID from(final String aGenreId, final String aCategoryId) {
+        return new GenreCategoryID(
+                aGenreId,
+                aCategoryId
+        );
+    }
+
+    public String getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(String genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final GenreCategoryID that = (GenreCategoryID) o;
+        return getGenreId().equals(that.getGenreId()) && getCategoryId().equals(that.getCategoryId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGenreId(), getCategoryId());
+    }
+}
---
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/genre/persistence/GenreJpaEntity.java
@@ -0,0 +1,150 @@
+package com.fullcyle.admin.catalog.infastructure.genre.persistence;
+
+import com.fullcyle.admin.catalog.domain.category.CategoryID;
+import com.fullcyle.admin.catalog.domain.genre.Genre;
+import com.fullcyle.admin.catalog.domain.genre.GenreID;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.EAGER;
+
+@Entity
+@Table(name = "GENRES")
+public class GenreJpaEntity {
+
+    @Id
+    @Column(name = "ID", updatable = false)
+    private String id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "FL_ACTIVE", nullable = false)
+    private boolean active;
+
+    @OneToMany(mappedBy = "genre", cascade = ALL, fetch = EAGER, orphanRemoval = true)
+    private Set<GenreCategoryJpaEntity> categories;
+
+    @Column(name = "CREATED_AT", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant createdAt;
+
+    @Column(name = "UPDATED_AT", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant updatedAt;
+
+    @Column(name = "DELETED_AT", nullable = false, columnDefinition = "DATETIME(6)")
+    private Instant deletedAt;
+
+    public GenreJpaEntity() {
+    }
+
+    private GenreJpaEntity(final String anId,
+                           final String aName,
+                           final boolean isActive,
+                           final Instant createdAt,
+                           final Instant updatedAt,
+                           final Instant deletedAt) {
+        this.id = anId;
+        this.name = aName;
+        this.active = isActive;
+        this.categories = new HashSet<>();
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static GenreJpaEntity from(final Genre aGenre) {
+        final var anEntity = new GenreJpaEntity(
+                aGenre.getId().getValue(),
+                aGenre.getName(),
+                aGenre.isActive(),
+                aGenre.getCreatedAt(),
+                aGenre.getUpdatedAt(),
+                aGenre.getDeletedAt()
+        );
+
+        aGenre.getCategories()
+                .forEach(anEntity::addCategory);
+
+        return anEntity;
+    }
+
+    public Genre toAggreate() {
+        return Genre.with(
+                GenreID.from(getId()),
+                getName(),
+                isActive(),
+                getCategories().stream().map(it -> CategoryID.from(it.getId().getCategoryId())).toList(),
+                getCreatedAt(),
+                getUpdatedAt(),
+                getDeletedAt()
+        );
+    }
+
+    private void addCategory(final CategoryID anId) {
+        this.categories.add(GenreCategoryJpaEntity.from(this, anId));
+    }
+
+    private void removeCategory(final CategoryID anId) {
+        this.categories.remove(GenreCategoryJpaEntity.from(this, anId));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<GenreCategoryJpaEntity> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<GenreCategoryJpaEntity> categories) {
+        this.categories = categories;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Instant deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}

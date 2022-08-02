39bc99ecb7f08b8a131f0ff303867dc0b775512a
joseMarciano <marcianojosepaulo@gmail.com>
2022-08-01 22:42:21 -0300

    test: :rotating_light: integration test about create Genre end-point

---
+++ infastructure/src/test/java/com/fullcyle/admin/catalog/infastructure/api/GenreAPITest.java
@@ -0,0 +1,111 @@
+package com.fullcyle.admin.catalog.infastructure.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fullcyle.admin.catalog.ControllerTest;
+import com.fullcyle.admin.catalog.application.genre.create.CreateGenreOutput;
+import com.fullcyle.admin.catalog.application.genre.create.CreateGenreUseCase;
+import com.fullcyle.admin.catalog.domain.exceptions.NotificationException;
+import com.fullcyle.admin.catalog.domain.validation.handler.Notification;
+import com.fullcyle.admin.catalog.infastructure.genre.models.CreateGenreRequest;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.mockito.Mockito.*;
+
+@ControllerTest(controllers = GenreAPI.class)
+public class GenreAPITest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private CreateGenreUseCase createGenreUseCase;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateGenre_shouldReturnGenreId() throws Exception {
+        // given
+        final var expectedName = "Ação";
+        final var expectedCategories = List.of("123", "456");
+        final var expectedId = "123";
+        final var expectedIsActive = true;
+
+        final var aCommand = new CreateGenreRequest(expectedName, expectedCategories, expectedIsActive);
+
+        when(createGenreUseCase.execute(any()))
+                .thenReturn(CreateGenreOutput.from(expectedId));
+
+        // when
+        final var aRequest = MockMvcRequestBuilders.post("/genres")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aCommand));
+
+        final var aResponse = this.mvc.perform(aRequest)
+                .andDo(MockMvcResultHandlers.print());
+
+
+        // then
+        aResponse.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.header().string("Location", "/genres/" + expectedId))
+                .andExpect(MockMvcResultMatchers.header().string("Content-type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.equalTo(expectedId)));
+
+        verify(createGenreUseCase).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedCategories, cmd.categories())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+
+    }
+
+    @Test
+    public void givenAInValidCommand_whenCallsCreateGenre_shouldReturnNotification() throws Exception {
+        // given
+        final String expectedName = null;
+        final var expectedCategories = List.of("123", "456");
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = new CreateGenreRequest(expectedName, expectedCategories, expectedIsActive);
+
+        when(createGenreUseCase.execute(any()))
+                .thenThrow(new NotificationException("Error", Notification.create(new Error(expectedErrorMessage))));
+
+        // when
+        final var aRequest = MockMvcRequestBuilders.post("/genres")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aCommand));
+
+        final var aResponse = this.mvc.perform(aRequest)
+                .andDo(MockMvcResultHandlers.print());
+
+
+        // then
+        aResponse.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity())
+                .andExpect(MockMvcResultMatchers.header().string("Location", Matchers.nullValue()))
+                .andExpect(MockMvcResultMatchers.header().string("Content-type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors[0].message", Matchers.equalTo(expectedErrorMessage)))
+        ;
+
+        verify(createGenreUseCase).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedCategories, cmd.categories())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+
+    }
+
+}

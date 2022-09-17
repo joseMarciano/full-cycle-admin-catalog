3bb1cee7b420bda79a6819e479475d29ea1fb011
joseMarciano <marcianojosepaulo@gmail.com>
2022-09-17 11:24:03 -0300

    feat: :sparkles: implements CastMemberValidator

--- domain/src/main/java/com/fullcyle/admin/catalog/domain/castmember/CastMemberValidator.java
+++ domain/src/main/java/com/fullcyle/admin/catalog/domain/castmember/CastMemberValidator.java
@@ -1,10 +1,13 @@
 package com.fullcyle.admin.catalog.domain.castmember;
 
+import com.fullcyle.admin.catalog.domain.validation.Error;
 import com.fullcyle.admin.catalog.domain.validation.ValidationHandler;
 import com.fullcyle.admin.catalog.domain.validation.Validator;
 
 public class CastMemberValidator extends Validator {
 
+    private static final int NAME_MAX_LENGTH = 255;
+    private static final int NAME_MIN_LENGTH = 1;
     private final CastMember castMember;
 
     public CastMemberValidator(final ValidationHandler anHandler, final CastMember aMember) {
@@ -14,6 +17,37 @@ public class CastMemberValidator extends Validator {
 
     @Override
     public void validate() {
+        checkNameConstraints();
+        checkTypeConstraints();
+    }
+
+    private void checkNameConstraints() {
+        final var name = this.castMember.getName();
+
+        if (name == null) {
+            this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if (name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final int length = name.trim().length();
+        if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
+            this.validationHandler().append(new Error("'name' must be between 1 and 255 characters"));
+        }
+
+    }
+
+    private void checkTypeConstraints() {
+        final var type = this.castMember.getType();
+
+        if (type == null) {
+            this.validationHandler().append(new Error("'type' should not be null"));
+            return;
+        }
 
     }
 }

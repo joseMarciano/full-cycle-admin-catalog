1cbe775cb483cc0be5a707c98879a951b8c1588c
joseMarciano <marcianojosepaulo@gmail.com>
2022-10-02 21:52:36 -0300

    fix: :bug: add @JsonProperty for mapper work correctly with record

--- infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/castmember/models/CastMemberResponse.java
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/castmember/models/CastMemberResponse.java
@@ -1,5 +1,6 @@
 package com.fullcyle.admin.catalog.infastructure.castmember.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fullcyle.admin.catalog.domain.castmember.CastMemberType;
 
 import java.time.Instant;
@@ -8,7 +9,7 @@ public record CastMemberResponse(
         String id,
         String name,
         CastMemberType type,
-        Instant createdAt,
-        Instant updatedAt
+        @JsonProperty("created_at") Instant createdAt,
+        @JsonProperty("updated_at") Instant updatedAt
 ) {
 }

6ebb2e1898bf58cebfc61ef21fdd4d6c85d2f475
joseMarciano <marcianojosepaulo@gmail.com>
2022-10-12 20:32:09 -0300

    test: :test_tube: tests about update Video

--- domain/src/test/java/com/fullcyle/admin/catalog/domain/video/VideoTest.java
+++ domain/src/test/java/com/fullcyle/admin/catalog/domain/video/VideoTest.java
@@ -72,4 +72,417 @@ public class VideoTest {
 
     }
 
+    @Test
+    public void givenValidVideo_whenCallsUpdate_shouldReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                These questions, duly considered, raise all doubts about the mobility
+                of international capitals, challenging the ability to equalize the
+                intended indices. Likewise, the phenomenon of the Internet is not mandatory
+                for the analysis of the communication process as a whole. Above all,
+                it is essential that consultation with the various activists can increase
+                """;
+        final var expectedLauchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                "Any title",
+                "Any description",
+                Year.of(1999),
+                0.0,
+                true,
+                true,
+                Rating.AGE_10,
+                Set.of(),
+                Set.of(),
+                Set.of()
+        );
+
+        final var actualVideo = Video.with(aVideo)
+                .update(
+                        expectedTitle,
+                        expectedDescription,
+                        expectedLauchedAt,
+                        expectedDuration,
+                        expectedOpened,
+                        expectedPublished,
+                        expectedRating,
+                        expectedCategories,
+                        expectedGenres,
+                        expectedMembers
+                );
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(aVideo.getId(), actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLauchedAt, actualVideo.getLauchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+
+        Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetVideo_shouldReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                These questions, duly considered, raise all doubts about the mobility
+                of international capitals, challenging the ability to equalize the
+                intended indices. Likewise, the phenomenon of the Internet is not mandatory
+                for the analysis of the communication process as a whole. Above all,
+                it is essential that consultation with the various activists can increase
+                """;
+        final var expectedLauchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLauchedAt,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aVideoMedia = AudioVideoMedia.with(
+                "abc",
+                "Video.mp4",
+                "/123/videos",
+                "",
+                MediaStatus.PENDING
+        );
+
+        final var actualVideo =
+                Video.with(aVideo)
+                        .setVideo(aVideoMedia);
+
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(aVideo.getId(), actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLauchedAt, actualVideo.getLauchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertEquals(aVideoMedia, actualVideo.getVideo().get());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+
+        Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetTrailer_shouldReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                These questions, duly considered, raise all doubts about the mobility
+                of international capitals, challenging the ability to equalize the
+                intended indices. Likewise, the phenomenon of the Internet is not mandatory
+                for the analysis of the communication process as a whole. Above all,
+                it is essential that consultation with the various activists can increase
+                """;
+        final var expectedLauchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLauchedAt,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aTrailer = AudioVideoMedia.with(
+                "abc",
+                "Video.mp4",
+                "/123/videos",
+                "",
+                MediaStatus.PENDING
+        );
+
+        final var actualVideo =
+                Video.with(aVideo)
+                        .setTrailer(aTrailer);
+
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(aVideo.getId(), actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLauchedAt, actualVideo.getLauchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertEquals(aTrailer, actualVideo.getTrailer().get());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+
+        Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetBanner_shouldReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                These questions, duly considered, raise all doubts about the mobility
+                of international capitals, challenging the ability to equalize the
+                intended indices. Likewise, the phenomenon of the Internet is not mandatory
+                for the analysis of the communication process as a whole. Above all,
+                it is essential that consultation with the various activists can increase
+                """;
+        final var expectedLauchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLauchedAt,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aBanner = ImageMedia.with(
+                "abc",
+                "Image.jpg",
+                "/123/images"
+        );
+
+        final var actualVideo =
+                Video.with(aVideo)
+                        .setBanner(aBanner);
+
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(aVideo.getId(), actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLauchedAt, actualVideo.getLauchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertEquals(aBanner, actualVideo.getBanner().get());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+
+        Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetThumbNail_shouldReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                These questions, duly considered, raise all doubts about the mobility
+                of international capitals, challenging the ability to equalize the
+                intended indices. Likewise, the phenomenon of the Internet is not mandatory
+                for the analysis of the communication process as a whole. Above all,
+                it is essential that consultation with the various activists can increase
+                """;
+        final var expectedLauchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLauchedAt,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aThumbNail = ImageMedia.with(
+                "abc",
+                "Image.jpg",
+                "/123/images"
+        );
+
+        final var actualVideo =
+                Video.with(aVideo)
+                        .setThumbnail(aThumbNail);
+
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(aVideo.getId(), actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLauchedAt, actualVideo.getLauchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertEquals(aThumbNail, actualVideo.getThumbnail().get());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+
+        Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetThumbNailHalf_shouldReturnUpdated() {
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+                These questions, duly considered, raise all doubts about the mobility
+                of international capitals, challenging the ability to equalize the
+                intended indices. Likewise, the phenomenon of the Internet is not mandatory
+                for the analysis of the communication process as a whole. Above all,
+                it is essential that consultation with the various activists can increase
+                """;
+        final var expectedLauchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLauchedAt,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aThumbNailHalf = ImageMedia.with(
+                "abc",
+                "Image.jpg",
+                "/123/images"
+        );
+
+        final var actualVideo =
+                Video.with(aVideo)
+                        .setThumbnailHalf(aThumbNailHalf);
+
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertEquals(aVideo.getId(), actualVideo.getId());
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLauchedAt, actualVideo.getLauchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration());
+        assertEquals(expectedOpened, actualVideo.getOpened());
+        assertEquals(expectedPublished, actualVideo.getPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertEquals(aThumbNailHalf, actualVideo.getThumbnailHalf().get());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+
+        Assertions.assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+
+    }
+
 }

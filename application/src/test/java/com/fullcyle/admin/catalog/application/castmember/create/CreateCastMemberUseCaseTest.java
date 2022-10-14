f6a6ae25ebe8530ef8aa759e9377ce7d5170c6be
joseMarciano <marcianojosepaulo@gmail.com>
2022-10-13 22:56:38 -0300

    refactor: :hammer: change class names

--- application/src/test/java/com/fullcyle/admin/catalog/application/castmember/create/CreateCastMemberUseCaseTest.java
+++ application/src/test/java/com/fullcyle/admin/catalog/application/castmember/create/CreateCastMemberUseCaseTest.java
@@ -36,7 +36,7 @@ public class CreateCastMemberUseCaseTest extends UseCaseTest {
     public void givenAValidCommand_whenCallsCreateCastMember_shouldReturnIt() {
         //given
         final var expectedName = Fixture.name();
-        final var expectedType = Fixture.CastMember.type();
+        final var expectedType = Fixture.CastMembers.type();
 
         final var aCommnad = CreateCastMemberCommand.with(
                 expectedName,
@@ -68,7 +68,7 @@ public class CreateCastMemberUseCaseTest extends UseCaseTest {
     public void givenAInvalidName_whenCallsCreateCastMember_shouldThrowsNotificationException() {
         //given
         final String expectedName = null;
-        final var expectedType = Fixture.CastMember.type();
+        final var expectedType = Fixture.CastMembers.type();
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = "'name' should not be null";
--- application/src/test/java/com/fullcyle/admin/catalog/application/castmember/retrieve/list/ListCastMemberUseCaseTest.java
+++ application/src/test/java/com/fullcyle/admin/catalog/application/castmember/retrieve/list/ListCastMemberUseCaseTest.java
@@ -34,8 +34,8 @@ public class ListCastMemberUseCaseTest extends UseCaseTest {
     public void givenAValidQuery_whenCallsListCastMember_shouldReturnCastMembers() {
         //given
         final var castMembers = List.of(
-                CastMember.newMember(Fixture.name(), Fixture.CastMember.type()),
-                CastMember.newMember(Fixture.name(), Fixture.CastMember.type())
+                CastMember.newMember(Fixture.name(), Fixture.CastMembers.type()),
+                CastMember.newMember(Fixture.name(), Fixture.CastMembers.type())
         );
 
         final var expectedPage = 0;

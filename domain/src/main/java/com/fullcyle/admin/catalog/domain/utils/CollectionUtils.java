70d07cdb2a7b556d94ef9542fb561933017f37d6
joseMarciano <marcianojosepaulo@gmail.com>
2022-10-15 14:43:35 -0300

    chore: :wrench: create overload for mapTo

--- domain/src/main/java/com/fullcyle/admin/catalog/domain/utils/CollectionUtils.java
+++ domain/src/main/java/com/fullcyle/admin/catalog/domain/utils/CollectionUtils.java
@@ -1,5 +1,6 @@
 package com.fullcyle.admin.catalog.domain.utils;
 
+import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -9,10 +10,11 @@ public final class CollectionUtils {
     private CollectionUtils() {
     }
 
-    ;
-
-
     public static <IN, OUT> Set<OUT> mapTo(final Set<IN> set, Function<IN, OUT> mapper) {
         return set.stream().map(mapper).collect(Collectors.toSet());
+    }
+
+    public static <IN, OUT> List<OUT> mapTo(final List<IN> set, Function<IN, OUT> mapper) {
+        return set.stream().map(mapper).toList();
     }
 }

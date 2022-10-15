e83b6d2ee919adb7a7181893cf7ec96885fa960c
joseMarciano <marcianojosepaulo@gmail.com>
2022-10-15 10:19:13 -0300

    test: :test_tube: implements tests about DeleteVideoUseCase

---
+++ application/src/test/java/com/fullcyle/admin/catalog/application/video/delete/DeleteVideoUseCaseTest.java
@@ -0,0 +1,70 @@
+package com.fullcyle.admin.catalog.application.video.delete;
+
+import com.fullcyle.admin.catalog.application.UseCaseTest;
+import com.fullcyle.admin.catalog.domain.exceptions.InternalErrorException;
+import com.fullcyle.admin.catalog.domain.video.VideoGateway;
+import com.fullcyle.admin.catalog.domain.video.VideoID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class DeleteVideoUseCaseTest extends UseCaseTest {
+
+    @InjectMocks
+    private DefaultDeleteVideoUseCase useCase;
+
+    @Mock
+    private VideoGateway videoGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return null;
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteVideo_shouldDeleteIt() {
+
+        final var expectedId = VideoID.unique();
+
+        doNothing()
+                .when(videoGateway).deleteById(any());
+
+        Assertions.assertDoesNotThrow(() -> this.useCase.execute(expectedId.getValue()));
+
+        verify(videoGateway).deleteById(eq(expectedId));
+
+    }
+
+    @Test
+    public void givenAnInvalidId_whenCallsDeleteVideo_shouldBeOk() {
+
+        final var expectedId = VideoID.from("123");
+
+        doNothing()
+                .when(videoGateway).deleteById(any());
+
+        Assertions.assertDoesNotThrow(() -> this.useCase.execute(expectedId.getValue()));
+
+        verify(videoGateway).deleteById(eq(expectedId));
+
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteVideoAndGatewayThrowsException_shouldReceiveException() {
+
+        final var expectedId = VideoID.from("123");
+
+        doThrow(InternalErrorException.with("Error on delete video", new RuntimeException()))
+                .when(videoGateway).deleteById(any());
+
+        Assertions.assertThrows(InternalErrorException.class, () -> this.useCase.execute(expectedId.getValue()));
+
+        verify(videoGateway).deleteById(eq(expectedId));
+
+    }
+}

945f1e4ba428913e3c4cf2972d7ed25875e2e691
joseMarciano <marcianojosepaulo@gmail.com>
2023-05-23 20:55:12 -0300

    chore: :wrench: put profile on InMemoryStorageService

--- infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/services/InMemoryStorageService.java
+++ infastructure/src/main/java/com/fullcyle/admin/catalog/infastructure/services/InMemoryStorageService.java
@@ -1,6 +1,7 @@
 package com.fullcyle.admin.catalog.infastructure.services;
 
 import com.fullcyle.admin.catalog.domain.video.Resource;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Profile({"test-e2e", "test-integration"})
 public class InMemoryStorageService implements StorageService { // THIS CLASS WAS CREATED TO SIMULATES S3 TESTS
 
     @Override
@@ -19,7 +21,9 @@ public class InMemoryStorageService implements StorageService { // THIS CLASS WA
     public Optional<Resource> get(final String name) {
         return Optional.empty();
     }
-
+//8ed354f3b7094655ac012ccbc8cb83fb category
+//    fbaf2273c11543e1853e92ad0394d7e2 genre
+//            67e2afea82b44b74bf077e87c7214d22 cast_member
 
     @Override
     public List<String> list(final String prefix) {
